7314e195d6d4a419f250b6d73d6f7e3698d73eab
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-08-13 23:08:01 +0200

    initialization of messenger page

---
+++ client/src/pages/messenger/Messenger.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import "./messenger.css";
+
+export default function Messenger() {
+    return (
+        <div className="messenger">
+            hello
+        </div>
+    )
+}
