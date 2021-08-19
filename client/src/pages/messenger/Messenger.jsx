eb3de4140ee95363bd378a8f7a436ec28bf84f44
BillBenon <61540696+BillBenon@users.noreply.github.com>
2021-08-20 01:09:55 +0200

    addUser and getUsers emit and on events on client done

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -15,19 +15,16 @@ export default function Messenger() {
   const [currentChat, setCurrentChat] = useState(null);
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
-  const [socket, setSocket] = useState(null);
+  const socket = useRef(io("ws://localhost:8900"));
   const { user } = useContext(AuthContext);
   const scrollRef = useRef();
 
-  useEffect(() => {
-    setSocket(io("ws://localhost:8900"));
-  }, []);
-
-  useEffect(() => {
-    socket?.on("welcome", (message) => {
-      console.log(message);
+  useRef(() => {
+    socket.current.emit("addUser", user._id);
+    socket.current.on("getUsers", (users) => {
+      console.log(users);
     });
-  }, [socket]);
+  }, [user]);
 
   useEffect(() => {
     const getConversations = async () => {
